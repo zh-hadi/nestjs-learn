d1f63b092742e6df8a28ddc4b04b95ac33cc9325
Hadiuzzaman <zhhadi50@gmail.com>
2026-04-27 19:22:34 +0600

    method and status code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+
+class CreateUserDto {
+    name: string;
+    email: string;
+    age: Number;
+}
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +14,22 @@ export class UsersController {
     @Get()
     getAllUsers(){
         return this.usersService.getAll()
+    }
+
+    @Post()
+    @HttpCode(404)
+    create(@Body() createUserDto: CreateUserDto) {
+        console.log(createUserDto)
+        return "create new users";
+    }
+
+    @Get("query")
+    getQuery(@Query('name') name: string) {
+        return name;
+    }
+
+    @Get(":id")
+    findUser(@Param('id') id: string){
+        return this.usersService.findById(Number(id))
     }
 }
